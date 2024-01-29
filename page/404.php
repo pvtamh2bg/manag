03b2b02306772f012306b625d24da4d5d9e5bef7
tamtmh <tampv@tmh-techlab.vn>
2024-01-29 14:28:42 +0700

    add 404 not found

---
+++ page/404.php
@@ -0,0 +1,612 @@
+<!DOCTYPE html>
+<!-- saved from url=(0038)https://mangaplus.shueisha.co.jp/error -->
+<html prefix="og: http://ogp.me/ns#" lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link
+      rel="shortcut icon"
+      href="https://mangaplus.shueisha.tv/page/frontend/images/favicon.png"
+    />
+    <title>MANGA Plus by SHUEISHA</title>
+    <meta name="format-detection" content="telephone=no, email=no" />
+    <link rel="preconnect" href="https://jumpg-webapi.tokyo-cdn.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="MANGA Plus by SHUEISHA TV" />
+    <meta property="og:locale" content="en_us" />
+    <meta property="og:image" content="https://mangaplus.shueisha.tv/page/frontend/images/favicon.png" />
+    <meta property="og:image:alt" content="Logo for MANGA Plus Shueisha TV" />
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com/"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="https://mangaplus.shueisha.tv/page/frontend/images/favicon.png"
+    />
+    <meta name="theme-color" content="#5d0914" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-mobile-web-app-title" content="MANGA Plus by Shueisha TV" />
+    <meta name="msapplication-TileColor" content="#000000" />
+    <style type="text/css">
+      body {
+        background-color: #191919;
+        color: #fff;
+        font-family: Roboto, sans-serif;
+        font-weight: 500;
+        max-width: 100%;
+        -webkit-overflow-scrolling: touch;
+      }
+      html {
+        overflow-x: hidden;
+      }
+      .Error-module_container_mzmCx {
+        width: 100%;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        text-align: center;
+      }
+      h2 {
+        position: relative;
+        display: block;
+        margin: 50px auto 200px auto;
+        font-size: 32px;
+        text-transform: uppercase;
+        color: #12000a;
+        font-weight: 300;
+        font-family: Audiowide;
+        text-shadow: 0 0 4px #12000a;
+        -webkit-animation: Error-module_fadeInText_3Fsu1 3s ease-in 3.5s
+            forwards,
+          Error-module_flicker4_21GXI 5s linear 7.5s infinite,
+          Error-module_hueRotate_QjA-l 6s ease-in-out 3s infinite;
+        animation: Error-module_fadeInText_3Fsu1 3s ease-in 3.5s forwards,
+          Error-module_flicker4_21GXI 5s linear 7.5s infinite,
+          Error-module_hueRotate_QjA-l 6s ease-in-out 3s infinite;
+        font-family: Roboto,sans-serif;
+        width: 100%;
+      }
+      #Error-module_svgWrap_1_3UF8g,
+      #Error-module_svgWrap_2_2X6yY {
+        position: absolute;
+        height: auto;
+        max-width: 100%;
+        padding: 100px 20%;
+        -webkit-animation: Error-module_hueRotate_QjA-l 6s ease-in-out 3s
+          infinite;
+        animation: Error-module_hueRotate_QjA-l 6s ease-in-out 3s infinite;
+      }
+      #Error-module_id1_1_2HyJN,
+      #Error-module_id2_1_3-gle,
+      #Error-module_id3_1_3egtZ,
+      #Error-module_id4_1_3OXwS {
+        stroke: #ff005d;
+        stroke-width: 3px;
+        fill: transparent;
+      }
+      #Error-module_id1_2_1N161,
+      #Error-module_id2_2_QpOX7,
+      #Error-module_id3_2_2Wqwn,
+      #Error-module_id4_2_32ISq {
+        stroke: #12000a;
+        stroke-width: 3px;
+        fill: transparent;
+      }
+      #Error-module_id4_1_3OXwS {
+        stroke-dasharray: 940px;
+        stroke-dashoffset: -940px;
+        -webkit-animation: Error-module_drawLine3_1371q 3.5s ease-in-out 0s
+            forwards,
+          Error-module_flicker3_QDFsR 5s linear 5s infinite;
+        animation: Error-module_drawLine3_1371q 3.5s ease-in-out 0s forwards,
+          Error-module_flicker3_QDFsR 5s linear 5s infinite;
+      }
+      #Error-module_id3_1_3egtZ {
+        stroke-dasharray: 940px;
+        stroke-dashoffset: -940px;
+        -webkit-animation: Error-module_drawLine3_1371q 2.5s ease-in-out 0s
+            forwards,
+          Error-module_flicker3_QDFsR 4s linear 4s infinite;
+        animation: Error-module_drawLine3_1371q 2.5s ease-in-out 0s forwards,
+          Error-module_flicker3_QDFsR 4s linear 4s infinite;
+      }
+      #Error-module_id2_1_3-gle {
+        stroke-dasharray: 800px;
+        stroke-dashoffset: -800px;
+        -webkit-animation: Error-module_drawLine2_1GMtq 2.5s ease-in-out 0.5s
+            forwards,
+          Error-module_flicker2_1-LSG 4s linear 4.5s infinite;
+        animation: Error-module_drawLine2_1GMtq 2.5s ease-in-out 0.5s forwards,
+          Error-module_flicker2_1-LSG 4s linear 4.5s infinite;
+      }
+      #Error-module_id1_1_2HyJN {
+        stroke-dasharray: 940px;
+        stroke-dashoffset: -940px;
+        -webkit-animation: Error-module_drawLine1_3QcYt 2.5s ease-in-out 1s
+            forwards,
+          Error-module_flicker1_3-Lyi 4s linear 5s infinite;
+        animation: Error-module_drawLine1_3QcYt 2.5s ease-in-out 1s forwards,
+          Error-module_flicker1_3-Lyi 4s linear 5s infinite;
+      }
+      @-webkit-keyframes Error-module_drawLine1_3QcYt {
+        0% {
+          stroke-dashoffset: -940px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @keyframes Error-module_drawLine1_3QcYt {
+        0% {
+          stroke-dashoffset: -940px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @-webkit-keyframes Error-module_drawLine2_1GMtq {
+        0% {
+          stroke-dashoffset: -800px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @keyframes Error-module_drawLine2_1GMtq {
+        0% {
+          stroke-dashoffset: -800px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @-webkit-keyframes Error-module_drawLine3_1371q {
+        0% {
+          stroke-dashoffset: -940px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @keyframes Error-module_drawLine3_1371q {
+        0% {
+          stroke-dashoffset: -940px;
+        }
+        to {
+          stroke-dashoffset: 0px;
+        }
+      }
+      @-webkit-keyframes Error-module_flicker1_3-Lyi {
+        0% {
+          stroke: #ff005d;
+        }
+        1% {
+          stroke: transparent;
+        }
+        3% {
+          stroke: transparent;
+        }
+        4% {
+          stroke: #ff005d;
+        }
+        6% {
+          stroke: #ff005d;
+        }
+        7% {
+          stroke: transparent;
+        }
+        13% {
+          stroke: transparent;
+        }
+        14% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @keyframes Error-module_flicker1_3-Lyi {
+        0% {
+          stroke: #ff005d;
+        }
+        1% {
+          stroke: transparent;
+        }
+        3% {
+          stroke: transparent;
+        }
+        4% {
+          stroke: #ff005d;
+        }
+        6% {
+          stroke: #ff005d;
+        }
+        7% {
+          stroke: transparent;
+        }
+        13% {
+          stroke: transparent;
+        }
+        14% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @-webkit-keyframes Error-module_flicker2_1-LSG {
+        0% {
+          stroke: #ff005d;
+        }
+        50% {
+          stroke: #ff005d;
+        }
+        51% {
+          stroke: transparent;
+        }
+        61% {
+          stroke: transparent;
+        }
+        62% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @keyframes Error-module_flicker2_1-LSG {
+        0% {
+          stroke: #ff005d;
+        }
+        50% {
+          stroke: #ff005d;
+        }
+        51% {
+          stroke: transparent;
+        }
+        61% {
+          stroke: transparent;
+        }
+        62% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @-webkit-keyframes Error-module_flicker3_QDFsR {
+        0% {
+          stroke: #ff005d;
+        }
+        1% {
+          stroke: transparent;
+        }
+        10% {
+          stroke: transparent;
+        }
+        11% {
+          stroke: #ff005d;
+        }
+        40% {
+          stroke: #ff005d;
+        }
+        41% {
+          stroke: transparent;
+        }
+        45% {
+          stroke: transparent;
+        }
+        46% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @keyframes Error-module_flicker3_QDFsR {
+        0% {
+          stroke: #ff005d;
+        }
+        1% {
+          stroke: transparent;
+        }
+        10% {
+          stroke: transparent;
+        }
+        11% {
+          stroke: #ff005d;
+        }
+        40% {
+          stroke: #ff005d;
+        }
+        41% {
+          stroke: transparent;
+        }
+        45% {
+          stroke: transparent;
+        }
+        46% {
+          stroke: #ff005d;
+        }
+        to {
+          stroke: #ff005d;
+        }
+      }
+      @-webkit-keyframes Error-module_flicker4_21GXI {
+        0% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        30% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        31% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        32% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        36% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        37% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        41% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        42% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        85% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        86% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        95% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        96% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        to {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+      }
+      @keyframes Error-module_flicker4_21GXI {
+        0% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        30% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        31% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        32% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        36% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        37% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        41% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        42% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        85% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        86% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        95% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        96% {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+        to {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+      }
+      @-webkit-keyframes Error-module_fadeInText_3Fsu1 {
+        1% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        70% {
+          color: #ff005d;
+          text-shadow: 0 0 14px #ff005d;
+        }
+        to {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+      }
+      @keyframes Error-module_fadeInText_3Fsu1 {
+        1% {
+          color: #12000a;
+          text-shadow: 0 0 4px #12000a;
+        }
+        70% {
+          color: #ff005d;
+          text-shadow: 0 0 14px #ff005d;
+        }
+        to {
+          color: #ff005d;
+          text-shadow: 0 0 4px #ff005d;
+        }
+      }
+      @-webkit-keyframes Error-module_hueRotate_QjA-l {
+        0% {
+          -webkit-filter: hue-rotate(0deg);
+          filter: hue-rotate(0deg);
+        }
+        50% {
+          -webkit-filter: hue-rotate(-120deg);
+          filter: hue-rotate(-120deg);
+        }
+        to {
+          -webkit-filter: hue-rotate(0deg);
+          filter: hue-rotate(0deg);
+        }
+      }
+      @keyframes Error-module_hueRotate_QjA-l {
+        0% {
+          -webkit-filter: hue-rotate(0deg);
+          filter: hue-rotate(0deg);
+        }
+        50% {
+          -webkit-filter: hue-rotate(-120deg);
+          filter: hue-rotate(-120deg);
+        }
+        to {
+          -webkit-filter: hue-rotate(0deg);
+          filter: hue-rotate(0deg);
+        }
+      }
+      .styles-module_container_1rtol {
+        min-height: 100vh;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        -webkit-box-orient: vertical;
+        -webkit-box-direction: normal;
+        -ms-flex-direction: column;
+        flex-direction: column;
+      }
+      .styles-module_mainContainer_2tQWW {
+        margin: 0 auto 25px auto;
+        width: 100%;
+        padding-left: calc(var(--space) * 0);
+        padding-right: calc(var(--space) * 0);
+      }
+
+      @media screen and (min-width: 769px) {
+        .styles-module_mainContainer_2tQWW {
+          padding-left: calc(var(--space) * 0);
+          padding-right: calc(var(--space) * 0);
+        }
+      }
+      @media screen and (max-width: 768px) {
+        .styles-module_mainContainer_2tQWW {
+          margin: 25px auto 25px auto;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <noscript
+      ><strong
+        >We're sorry but MANGA Plus doesn't work properly without JavaScript
+        enabled. Please enable it to continue.</strong
+      ></noscript
+    >
+    <div id="app">
+      <div
+        class="vue-notification-group"
+        style="width: 300px; top: 0px; right: 0px"
+      >
+        <span></span>
+      </div>
+      <div>
+        <div class="styles-module_container_1rtol">
+          <div class="styles-module_mainContainer_2tQWW">
+            <div class="Error-module_container_mzmCx">
+              <div></div>
+              <h2>Page Not Found</h2>
+              <svg
+                id="Error-module_svgWrap_2_2X6yY"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 700 250"
+              >
+                <g>
+                  <path
+                    id="Error-module_id3_2_2Wqwn"
+                    d="M202.763,183.736v50.373H125.969V178.736H8.328V118.181L110.175,15.891h92.588V118.453H222.37v60.283H202.924ZM120,91L93.563,117.635h32.406V84.921Z"
+                  ></path>
+                  <path
+                    id="Error-module_id2_2_QpOX7"
+                    d="M231,211l47.237-4.936a112.842,112.842,0,1,0-21.246-28.908L222,212Z"
+                  ></path>
+                  <path
+                    id="Error-module_id1_2_1N161"
+                    d="M672.763,183.736v50.373H595.969V178.736H478.328V118.181L580.175,15.891h92.588V118.453H692.37v60.283H672.924ZM590,91l-26.437,26.635h32.406V84.921Z"
+                  ></path>
+                  <path
+                    id="Error-module_id4_2_32ISq"
+                    d="M270.412,121.56a14.628,14.628,0,0,1,14.623-14.633H340.69V51.354a14.627,14.627,0,0,1,14.622-14.633h5.624a14.628,14.628,0,0,1,14.622,14.633v55.573h55.7a14.628,14.628,0,0,1,14.622,14.633v5.628a14.627,14.627,0,0,1-14.622,14.633h-55.7v55.864a14.628,14.628,0,0,1-14.622,14.633h-5.624a14.628,14.628,0,0,1-14.622-14.633V141.821H285.035a14.628,14.628,0,0,1-14.623-14.633V121.56Z"
+                  ></path>
+                </g></svg
+              ><svg
+                id="Error-module_svgWrap_1_3UF8g"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 700 250"
+              >
+                <g>
+                  <path
+                    id="Error-module_id3_1_3egtZ"
+                    d="M202.763,183.736v50.373H125.969V178.736H8.328V118.181L110.175,15.891h92.588V118.453H222.37v60.283H202.924ZM120,91L93.563,117.635h32.406V84.921Z"
+                  ></path>
+                  <path
+                    id="Error-module_id2_1_3-gle"
+                    d="M231,211l47.237-4.936a112.842,112.842,0,1,0-21.246-28.908L222,212Z"
+                  ></path>
+                  <path
+                    id="Error-module_id1_1_2HyJN"
+                    d="M672.763,183.736v50.373H595.969V178.736H478.328V118.181L580.175,15.891h92.588V118.453H692.37v60.283H672.924ZM590,91l-26.437,26.635h32.406V84.921Z"
+                  ></path>
+                  <path
+                    id="Error-module_id4_1_3OXwS"
+                    d="M270.412,121.56a14.628,14.628,0,0,1,14.623-14.633H340.69V51.354a14.627,14.627,0,0,1,14.622-14.633h5.624a14.628,14.628,0,0,1,14.622,14.633v55.573h55.7a14.628,14.628,0,0,1,14.622,14.633v5.628a14.627,14.627,0,0,1-14.622,14.633h-55.7v55.864a14.628,14.628,0,0,1-14.622,14.633h-5.624a14.628,14.628,0,0,1-14.622-14.633V141.821H285.035a14.628,14.628,0,0,1-14.623-14.633V121.56Z"
+                  ></path>
+                </g>
+              </svg>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
