935fcf1711f46bcae8a5aa7cc550d18a6a643df0
tamtmh <tampv@tmh-techlab.vn>
2024-03-22 16:11:01 +0700

    add multi language index

---
+++ page/language/index.php
@@ -0,0 +1,495 @@
+<style>
+.styles-module_settingBtn_2xojh {
+    margin-left: 0;
+    position: absolute;
+    top: 5px;
+    right: 0;
+    z-index: 888;
+}
+@media (max-width: 768px) {
+    .styles-module_header_2_n3A {
+        padding:0
+    }
+
+    .styles-module_header_2_n3A a div {
+        text-align: center;
+        margin: 0 auto
+    }
+
+    .styles-module_header_2_n3A a {
+        width: 100%;
+        text-align: center;
+        margin: 0 20%
+    }
+
+    .styles-module_navigation_YdAIm,.styles-module_settingBtn_2xojh {
+        display: none
+    }
+
+    .styles-module_searchForm_rhCn- {
+        margin-right: 20px
+    }
+
+    .styles-module_hamburger_22tm_ {
+        margin: 0;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        padding: 30px 10px 0 30px;
+        z-index: 4
+    }
+
+    .styles-module_hamburger_22tm_ img {
+        height: 24px
+    }
+
+    .styles-module_modal_3F4WT {
+        display: none
+    }
+
+    .styles-module_menu_3I6fb {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex
+    }
+}
+
+.styles-module_languageSettingBtn_1MrGF {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    color: var(--color-white);
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: end;
+    -ms-flex-pack: end;
+    justify-content: flex-end;
+    cursor: pointer;
+    background: #5d0914;
+    background: -webkit-gradient(linear,right top,left top,from(#5d0914),color-stop(100%,rgba(93,9,20,0)),to(#5d0914));
+    background: linear-gradient(270deg,#5d0914,rgba(93,9,20,0) 100%,#5d0914 0);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#5d0914",endColorstr="#5d0914",GradientType=1);
+    height: 25px;
+    width: 350px;
+    z-index: 4;
+    -webkit-transition: all .1s;
+    transition: all .1s
+}
+.styles-module_languageSettingBtn_1MrGF:hover {
+    color: var(--color-yellow);
+    -webkit-transition: all .1s;
+    transition: all .1s
+}
+
+.styles-module_languageSettingBtn_1MrGF:hover .styles-module_triangle_7ngq6 {
+    background-color: rgba(0,0,0,.8);
+    padding: 5px 10px 0 10px
+}
+
+.styles-module_languageSettingBtn_1MrGF:hover .styles-module_text_NZjpR span {
+    background-color: rgba(0,0,0,.8);
+    color: var(--color-yellow);
+    -webkit-transition: all .1s;
+    transition: all .1s
+}
+
+.styles-module_languageSettingBtn_1MrGF:hover small {
+    color: var(--color-white);
+    -webkit-transition: all .1s;
+    transition: all .1s
+}
+
+.styles-module_triangle_7ngq6 {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    z-index: 4;
+    background-color: rgba(0,0,0,.5);
+    margin: 0;
+    -ms-flex-item-align: stretch;
+    align-self: stretch;
+    padding: 3px 10px 0 10px
+}
+
+.styles-module_triangle_7ngq6:after {
+    content: "";
+    display: inline-block;
+    width: 0;
+    height: 0;
+    border-left: var(--icon-size) solid transparent;
+    border-right: var(--icon-size) solid transparent;
+    border-top: var(--icon-size) solid var(--color-white);
+    padding-bottom: calc(var(--icon-size)/2);
+    z-index: 4;
+    -ms-flex-item-align: center;
+    align-self: center
+}
+
+.styles-module_languageSettingBtn_1MrGF:hover .styles-module_triangle_7ngq6:after {
+    border-top: var(--icon-size) solid var(--color-yellow);
+    position: relative
+}
+
+.styles-module_text_NZjpR {
+    font-size: 9px;
+    color: var(--color-small-impact);
+    letter-spacing: .1rem;
+    font-weight: 500;
+    text-align: right;
+    z-index: 4;
+    -ms-flex-item-align: stretch;
+    align-self: stretch;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex
+}
+
+.styles-module_text_NZjpR span {
+    font-size: 13px;
+    color: var(--color-white);
+    letter-spacing: 0;
+    background-color: rgba(0,0,0,.5);
+    margin: 0 0 0 5px;
+    border-right: 1px solid hsla(0,0%,100%,.1)
+}
+
+.styles-module_text_NZjpR span,small {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-item-align: stretch;
+    align-self: stretch;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-transition: all .1s;
+    transition: all .1s
+}
+
+small {
+    color: var(--color-small-impact);
+    font-size: 9px;
+    font-weight: 800;
+    letter-spacing: .05rem;
+    padding: 0 2px;
+    z-index: 4
+}
+
+.styles-module_text_NZjpR span b {
+    font-weight: 500;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    padding: 0 10px
+}
+
+@media (max-width: 768px) {
+    .styles-module_languageSettingBtn_1MrGF {
+        height:35px
+    }
+
+    .styles-module_text_NZjpR span {
+        border: 0;
+        margin: 0 0 0 20px
+    }
+
+    .styles-module_text_NZjpR span b {
+        padding: 0 15px 0 25px
+    }
+
+    .styles-module_languageSettingBtn_1MrGF:hover .styles-module_triangle_7ngq6,.styles-module_triangle_7ngq6 {
+        padding: 3px 20px 0 10px
+    }
+}
+
+@media (max-width: 460px) {
+    .styles-module_languageSettingBtn_1MrGF {
+        height:75px
+    }
+}
+
+.styles-module_header_2_n3A {
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    background-color: var(--color-red);
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    padding: 0 calc(var(--space)*4);
+    background: #dc0914;
+    background: -webkit-gradient(linear,left top,left bottom,from(rgba(220,9,20,.5)),color-stop(6%,#dc0914),color-stop(6%,rgba(0,0,0,.8)),to(rgba(220,9,20,0)));
+    background: linear-gradient(180deg,rgba(220,9,20,.5),#dc0914 6%,rgba(0,0,0,.8) 0,rgba(220,9,20,0));
+    -webkit-transition: all .3s;
+    transition: all .3s;
+    max-height: 82px
+}
+
+.styles-module_header_2_n3A:hover {
+    background: #dc0914;
+    background: -webkit-gradient(linear,left top,left bottom,from(rgba(220,9,20,.7)),color-stop(6%,#dc0914),color-stop(6%,#000),to(rgba(220,9,20,0)));
+    background: linear-gradient(180deg,rgba(220,9,20,.7),#dc0914 6%,#000 0,rgba(220,9,20,0));
+    -webkit-transition: all .3s;
+    transition: all .3s
+}
+
+.styles-module_header_2_n3A a div {
+    width: 150px;
+    position: relative;
+    top: 15px;
+    z-index: 2
+}
+
+.styles-module_header_2_n3A a div:after {
+    content: "";
+    background: url(../img/web_logo_190118_light-luffy.bcf5fcca.png) no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: inline-block;
+    left: 0;
+    top: 0;
+    -webkit-transform-origin: 30% 70%;
+    transform-origin: 30% 70%;
+    will-change: transform;
+    pointer-events: none;
+    -webkit-animation: styles-module_gomuback_20yQg .2s both;
+    animation: styles-module_gomuback_20yQg .2s both
+}
+
+.styles-module_header_2_n3A a:hover div:after {
+    -webkit-animation: styles-module_gomugomu_2sF1H 1s both;
+    animation: styles-module_gomugomu_2sF1H 1s both
+}
+
+@-webkit-keyframes styles-module_gomugomu_2sF1H {
+    0% {
+        -webkit-transform: skew(0deg,0deg);
+        transform: skew(0deg,0deg)
+    }
+
+    40% {
+        -webkit-transform: skew(-10deg,-10deg) scale(1);
+        transform: skew(-10deg,-10deg) scale(1)
+    }
+
+    50% {
+        -webkit-transform: skew(25deg,25deg) scale(1.75);
+        transform: skew(25deg,25deg) scale(1.75)
+    }
+
+    65% {
+        -webkit-transform: scale(1.2) skew(-5deg,-5deg);
+        transform: scale(1.2) skew(-5deg,-5deg)
+    }
+
+    75% {
+        -webkit-transform: scale(1.2) skew(5deg,5deg);
+        transform: scale(1.2) skew(5deg,5deg)
+    }
+
+    to {
+        -webkit-transform: scale(1.2) skew(0deg,0deg);
+        transform: scale(1.2) skew(0deg,0deg)
+    }
+}
+
+@keyframes styles-module_gomugomu_2sF1H {
+    0% {
+        -webkit-transform: skew(0deg,0deg);
+        transform: skew(0deg,0deg)
+    }
+
+    40% {
+        -webkit-transform: skew(-10deg,-10deg) scale(1);
+        transform: skew(-10deg,-10deg) scale(1)
+    }
+
+    50% {
+        -webkit-transform: skew(25deg,25deg) scale(1.75);
+        transform: skew(25deg,25deg) scale(1.75)
+    }
+
+    65% {
+        -webkit-transform: scale(1.2) skew(-5deg,-5deg);
+        transform: scale(1.2) skew(-5deg,-5deg)
+    }
+
+    75% {
+        -webkit-transform: scale(1.2) skew(5deg,5deg);
+        transform: scale(1.2) skew(5deg,5deg)
+    }
+
+    to {
+        -webkit-transform: scale(1.2) skew(0deg,0deg);
+        transform: scale(1.2) skew(0deg,0deg)
+    }
+}
+
+@-webkit-keyframes styles-module_gomuback_20yQg {
+    0% {
+        -webkit-transform: scale(1.2);
+        transform: scale(1.2)
+    }
+
+    to {
+        -webkit-transform: scale(1);
+        transform: scale(1)
+    }
+}
+
+@keyframes styles-module_gomuback_20yQg {
+    0% {
+        -webkit-transform: scale(1.2);
+        transform: scale(1.2)
+    }
+
+    to {
+        -webkit-transform: scale(1);
+        transform: scale(1)
+    }
+}
+
+
+
+</style>  
+
+<div class="styles-module_settingBtn_2xojh">
+   <div class="styles-module_languageSettingBtn_1MrGF">
+      <p class="styles-module_text_NZjpR" _size="s">LANGUAGE: <span><b>English</b></span></p>
+      <span class="styles-module_triangle_7ngq6"></span>
+   </div>
+</div>
+<div class="styles-module_container_Du5fK styles-module_setting_6Uurn" style="display: none;">
+   <div class="styles-module_divider_84j1N styles-module_divider_3mhyE"></div>
+   <div class="styles-module_languageWrap_2PNos">
+      <div class="styles-module_itemArea_1XSVI">
+         <h3 class="styles-module_text_2gkQu" _size="l">Service Language</h3>
+         <div class="styles-module_items_2sfN5">
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="en" checked="checked">
+               <label for="315ff4c2-en" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">English&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-en" checked="checked"><input type="radio" name="315ff4c2" id="315ff4c2-en" class="styles-module_input_1_xZD"><label for="315ff4c2-en" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="es">
+               <label for="315ff4c2-es" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Español&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-es"><input type="radio" name="315ff4c2" id="315ff4c2-es" class="styles-module_input_1_xZD"><label for="315ff4c2-es" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="th">
+               <label for="315ff4c2-th" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">ภาษาไทย&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-th"><input type="radio" name="315ff4c2" id="315ff4c2-th" class="styles-module_input_1_xZD"><label for="315ff4c2-th" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="pt">
+               <label for="315ff4c2-pt" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Português (BR)&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-pt"><input type="radio" name="315ff4c2" id="315ff4c2-pt" class="styles-module_input_1_xZD"><label for="315ff4c2-pt" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="id">
+               <label for="315ff4c2-id" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Bahasa (IND)&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-id"><input type="radio" name="315ff4c2" id="315ff4c2-id" class="styles-module_input_1_xZD"><label for="315ff4c2-id" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="ru">
+               <label for="315ff4c2-ru" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Русский&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-ru"><input type="radio" name="315ff4c2" id="315ff4c2-ru" class="styles-module_input_1_xZD"><label for="315ff4c2-ru" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="fr">
+               <label for="315ff4c2-fr" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Français&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-fr"><input type="radio" name="315ff4c2" id="315ff4c2-fr" class="styles-module_input_1_xZD"><label for="315ff4c2-fr" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="de">
+               <label for="315ff4c2-de" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Deutsch&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-de"><input type="radio" name="315ff4c2" id="315ff4c2-de" class="styles-module_input_1_xZD"><label for="315ff4c2-de" class="styles-module_radio_311JI"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="315ff4c2" domid="vi">
+               <label for="315ff4c2-vi" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF">Tiếng Việt&nbsp;&nbsp;</p>
+               </label>
+               <div name="315ff4c2" domid="315ff4c2-vi"><input type="radio" name="315ff4c2" id="315ff4c2-vi" class="styles-module_input_1_xZD"><label for="315ff4c2-vi" class="styles-module_radio_311JI"></label></div>
+            </div>
+         </div>
+      </div>
+      <div class="styles-module_itemArea_1XSVI">
+         <h3 class="styles-module_text_2gkQu" _size="l">Show Content</h3>
+         <div class="styles-module_items_2sfN5">
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="en" checked="checked" disabled="disabled">
+               <label for="contents-en" class="styles-module_label_3C1G0 styles-module_disabled_2TdUA">
+                  <p class="styles-module_l_2xDvF"><span>254 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-en" checked="checked" disabled="disabled"><input type="checkbox" disabled="disabled" name="contents" id="contents-en" class="styles-module_input_3SzMJ"><label for="contents-en" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="es">
+               <label for="contents-es" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>108 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-es"><input type="checkbox" name="contents" id="contents-es" class="styles-module_input_3SzMJ"><label for="contents-es" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="th">
+               <label for="contents-th" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>32 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-th"><input type="checkbox" name="contents" id="contents-th" class="styles-module_input_3SzMJ"><label for="contents-th" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="pt">
+               <label for="contents-pt" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>6 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-pt"><input type="checkbox" name="contents" id="contents-pt" class="styles-module_input_3SzMJ"><label for="contents-pt" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="id">
+               <label for="contents-id" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>8 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-id"><input type="checkbox" name="contents" id="contents-id" class="styles-module_input_3SzMJ"><label for="contents-id" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="ru">
+               <label for="contents-ru" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>11 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-ru"><input type="checkbox" name="contents" id="contents-ru" class="styles-module_input_3SzMJ"><label for="contents-ru" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="fr">
+               <label for="contents-fr" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>17 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-fr"><input type="checkbox" name="contents" id="contents-fr" class="styles-module_input_3SzMJ"><label for="contents-fr" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="de">
+               <label for="contents-de" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>2 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-de"><input type="checkbox" name="contents" id="contents-de" class="styles-module_input_3SzMJ"><label for="contents-de" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+            <div class="styles-module_wrapper_HMi7g styles-module_item_3w45u" name="contents" domid="vi">
+               <label for="contents-vi" class="styles-module_label_3C1G0 ">
+                  <p class="styles-module_l_2xDvF"><span>3 Series</span></p>
+               </label>
+               <div name="contents" domid="contents-vi"><input type="checkbox" name="contents" id="contents-vi" class="styles-module_input_3SzMJ"><label for="contents-vi" class="styles-module_checkbox_2pXpO"></label></div>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<script>
+  $('.styles-module_languageSettingBtn_1MrGF').on('click', function() {
+    $('.styles-module_setting_6Uurn').toggle();
+  })
+</script> 
